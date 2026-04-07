37267a039d435c41b26fd5eef6ebfa7c741bb24d
urnice1111 <emilianogarramxd@gmail.com>
2026-04-07 14:50:35 -0600

    POST: User register

--- app.js
+++ app.js
@@ -1,18 +1,64 @@
 const express = require('express')
+const bcrypt = require('bcrypt');
+const mysql = require('mysql2')
+
 const app = express()
 const port = 3000
 
-const mysql = require('mysql2')
+const { connect } = require('node:http2')
 
 const connection = mysql.createConnection({
   host: '127.0.0.1',
   port: 3307,
   user: 'root',
   password: '',
-  database: 'over_math'
+  database: 'redes'
 })
 
 connection.connect()
+
+app.use(express.json())
+
+
+app.post('/register', async (req, res) => {
+    try{
+      const {email, password} = req.body;
+
+      if (!email || !password) {
+        return res.status(400).json({
+          error: "email or password missing"
+        });
+      }
+
+      const salt_round = 10;
+      const hashedPassword = await bcrypt.hash(password, salt_round)
+
+      const sql = 'INSERT INTO cuenta (correo, contrasena_hash) VALUES (?, ?)';
+
+      connection.execute(sql, [email, hashedPassword], (err, result) => {
+        if(err){
+          if (err.code == 'ER_DUP_ENTRY'){
+            return res.status(409).json({
+              error : "user already exists"
+            });
+          }
+          console.error(err)
+          return res.status(500).json({
+            error: err.message
+          });
+        }
+
+        res.status(201).json({
+          message: "user created succesfully"
+        });
+      });
+    } catch(err){
+      console.error(err)
+      return res.status(500).json({
+        error: err.message
+      });
+    }
+});
 
 app.get('/get_questions/:island/:difficulty', (req, res) => {
     const { island, difficulty } = req.params;
